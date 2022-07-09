a75d1776558473b698852d26ec7bf98001f2855d
LynnLattYati <lynnlattyadi2410@gmail.com>
2022-07-09 15:50:42 +0630

    slider feature complete not with real data

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Slider from "./app/compontents/Slider";
+
+const data = [
+  {
+    id: "123",
+    author: "Admin",
+    title: "sanshwe",
+    content: "this is content",
+    thumbnail:
+      "https://images.unsplash.com/photo-1657299156537-f4bcdced5392?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: "1234",
+    author: "Admin",
+    title: "sanshwe",
+    content: "this is content",
+    thumbnail:
+      "https://images.unsplash.com/photo-1657299156185-6f5de6da0996?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  {
+    id: "1235",
+    author: "Admin",
+    title: "sanshwe",
+    content: "this is content",
+    thumbnail:
+      "https://res.cloudinary.com/dp2ngy0oq/image/upload/v1657281868/cjlvufbchrtoyobpaqam.jpg",
+  },
+  {
+    id: "1236",
+    author: "Admin",
+    title: "sanshwe",
+    content: "this is content",
+    thumbnail:
+      "https://images.unsplash.com/photo-1657244983736-0497fdc2f3d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+  },
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Slider data={data} title="Featured Posts" />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/compontents/Slider.js
@@ -0,0 +1,177 @@
+import react, { useState, useEffect, useRef } from "react";
+// import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Dimensions,
+  Image,
+} from "react-native";
+
+const width = Dimensions.get("window").width - 20;
+let intervalId;
+let currentSlideIndex = 0;
+
+export default function Slider({ data, title }) {
+  const [dataToRender, setDataToRender] = useState([]);
+  const [visibleSlideIndex, setVisibleSlideIndex] = useState(0);
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+
+  const flatList = useRef();
+
+  // set current index on slider change
+  const onViewableItemsChanged = useRef(({ viewableItems }) => {
+    currentSlideIndex = viewableItems[0]?.index || 0;
+    console.log("current slide index is -> ", currentSlideIndex);
+    setVisibleSlideIndex(currentSlideIndex);
+  });
+
+  const handleScrollTo = (index) => {
+    flatList.current.scrollToIndex({ animated: false, index });
+  };
+
+  const startSlider = () => {
+    if (currentSlideIndex <= dataToRender.length - 2) {
+      intervalId = setInterval(() => {
+        flatList.current.scrollToIndex({
+          animated: true,
+          index: currentSlideIndex + 1,
+        });
+      }, 2000);
+    } else {
+      console.log("pause");
+      pauseSlider();
+    }
+  };
+
+  const pauseSlider = () => {
+    clearInterval(intervalId);
+    console.log("pause << clear");
+  };
+
+  useEffect(() => {
+    if (dataToRender.length && flatList.current) {
+      startSlider();
+    }
+  }, [dataToRender.length]);
+
+  useEffect(() => {
+    const newData = [[...data].pop(), ...data, [...data].shift()];
+    setDataToRender([...newData]);
+  }, [data.length]);
+
+  useEffect(() => {
+    const length = dataToRender.length; // 6 featured posts including 2 clone posts
+    // reset slide to first
+    if (visibleSlideIndex === length - 1 && length) handleScrollTo(1);
+    // reset slide to last
+    if (visibleSlideIndex === 0 && length) handleScrollTo(length - 2);
+
+    const firtSlide = currentSlideIndex === 0;
+    const lastSlide = currentSlideIndex === length - 1;
+
+    // only four post visible
+    // set active slide index to show in slider indicator
+    if (lastSlide && length) setActiveSlideIndex(0);
+    // active first slider indicator when clone last post is activec
+    else if (firtSlide && length) setActiveSlideIndex(1);
+    // active last slider indicator when clone first post is activec
+    else setActiveSlideIndex(currentSlideIndex - 1); // otherwise set current slide index
+  }, [visibleSlideIndex]);
+
+  const renderItems = ({ item }) => {
+    return (
+      <View>
+        <Image source={{ uri: item.thumbnail }} style={styles.image} />
+        <View style={{ width }}>
+          <Text
+            numberOfLines={2}
+            style={[styles.postTitle, { fontWeight: "700", color: "#383838" }]}
+          >
+            {item.title}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.sliderHead}>
+        <Text style={styles.title}>{title}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <SlideIndicator data={data} activeSlideIndex={activeSlideIndex} />
+        </View>
+      </View>
+      <FlatList
+        ref={flatList}
+        data={dataToRender}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        initialScrollIndex={1}
+        getItemLayout={(_, index) => ({
+          length: width,
+          offset: width * index,
+          index,
+        })}
+        onViewableItemsChanged={onViewableItemsChanged.current}
+        keyExtractor={(item, index) => item.id + index}
+        onScrollBeginDrag={pauseSlider}
+        onScrollEndDrag={startSlider}
+        renderItem={renderItems}
+      />
+    </View>
+  );
+}
+
+const SlideIndicator = ({ data, activeSlideIndex }) =>
+  data.map((item, index) => {
+    return (
+      <View
+        key={item.id}
+        style={[
+          styles.slides,
+          {
+            backgroundColor:
+              activeSlideIndex === index ? "#393838" : "transparent",
+          },
+        ]}
+      />
+    );
+  });
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: "center",
+    width,
+    paddingTop: 50,
+  },
+  sliderHead: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 5,
+  },
+  title: { fontWeight: "700", color: "#383838", fontSize: 22 },
+  slides: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    borderWidth: 2,
+    marginLeft: 5,
+  },
+  image: {
+    width,
+    height: width / 1.7,
+    borderColor: "#fff",
+    borderRadius: 7,
+    backgroundColor: "gray",
+  },
+  postTitle: {
+    fontWeight: "700",
+    color: "#383838",
+    fontSize: 16,
+  },
+});
