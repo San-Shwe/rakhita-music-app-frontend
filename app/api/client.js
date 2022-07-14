2559c34a645d5961c8ab517a150a19576123858a
LynnLattYati <lynnlattyadi2410@gmail.com>
2022-07-14 13:19:31 +0630

    change api baseUrl

--- app/api/client.js
+++ app/api/client.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "http://192.168.1.11:8000/api" });
+const client = axios.create({
+  baseURL:
+    "https://rakhita-music-app.herokuapp.com/api/post/single/warbletoncouncil",
+}); //"http://yor_machine_ip:database_allow_port/api"
 
 export default client;
